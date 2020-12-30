eed6f90b66aa0a0b50c86a5ba2728b055dd51c91
Arasefe <ismail_yildirim69@yahoo.com>
2020-12-30 13:33:34 -0600

    30Dec2020

--- src/test/java/CommonIQs/_20HandlingSSLCertifications.java
+++ src/test/java/CommonIQs/_20HandlingSSLCertifications.java
@@ -1,5 +1,6 @@
 package CommonIQs;
 
+import com.sun.media.sound.SoftChorus;
 import io.github.bonigarcia.wdm.WebDriverManager;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
@@ -20,6 +21,20 @@ public class _20HandlingSSLCertifications {
         c.merge(ch);
         WebDriverManager.chromedriver().setup();
         WebDriver driver = new ChromeDriver(c);
+
+
+    }
+
+    public void handlingSSLCertifications1(){
+        DesiredCapabilities ch=DesiredCapabilities.chrome();
+        //ch.acceptInsecureCerts();
+        ch.setCapability(CapabilityType.ACCEPT_INSECURE_CERTS, true);
+        ch.setCapability(CapabilityType.ACCEPT_SSL_CERTS, true);
+        //Belows to your local browser
+        ChromeOptions c= new ChromeOptions();
+        c.merge(ch);
+        WebDriverManager.chromedriver().setup();
+        WebDriver driver=new ChromeDriver(c);
 
 
     }
