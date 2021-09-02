2baebec1b373815eda364acc618d45ee16067103
Arasefe <ismail_yildirim69@yahoo.com>
2021-09-02 07:14:07 -0500

    02Sept2021

--- src/test/java/CommonIQs/OpenURLWithoutGetNavigate.java
+++ src/test/java/CommonIQs/OpenURLWithoutGetNavigate.java
@@ -4,6 +4,7 @@ import io.github.bonigarcia.wdm.WebDriverManager;
 import org.openqa.selenium.JavascriptExecutor;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.Assert;
 import org.testng.annotations.BeforeClass;
 import org.testng.annotations.Test;
 
@@ -16,14 +17,40 @@ public class OpenURLWithoutGetNavigate {
     }
 
     @Test
-    public void openWithoutGetNavigate() {
+    public void openWithoutGetNavigate1() {
         // We need to downcast WebDriver reference variable to use JavascriptExecutor methods
         JavascriptExecutor js = (JavascriptExecutor) driver;
         String url = "https://www.amazon.com";
         String script = "window.location= \'"+url+"\'";
         js.executeScript(script);
+        System.out.println(driver.getCurrentUrl());
+        Assert.assertEquals(driver.getTitle(),"Amazon.com. Spend less. Smile more.");
+        driver.quit();
+    }
+
+    @Test
+    public void openWithoutGetNavigate2() {
+        // We need to downcast WebDriver reference variable to use JavascriptExecutor methods
+        JavascriptExecutor js = (JavascriptExecutor) driver;
+        String url = "https://www.google.com";
+        String script = "window.location=\'"+url+"\'";
+        js.executeScript(script);
 
         System.out.println(driver.getCurrentUrl());
+        Assert.assertEquals(driver.getTitle(),"Google");
+        driver.quit();
+    }
+
+    @Test
+    public void openWithoutGetNavigate3() {
+        // We need to downcast WebDriver reference variable to use JavascriptExecutor methods
+        JavascriptExecutor js = (JavascriptExecutor) driver;
+        String url = "https://www.carvana.com";
+        String script = "window.location=\'"+url+"\'";
+        js.executeScript(script);
+
+        System.out.println(driver.getCurrentUrl());
+        Assert.assertEquals(driver.getTitle(),"Carvana | Buy & Finance Used Cars Online | At Home Delivery");
         driver.quit();
     }
 }
---
+++ src/test/java/CommonIQs/Scroll.java
@@ -0,0 +1,75 @@
+package CommonIQs;
+
+import io.github.bonigarcia.wdm.WebDriverManager;
+
+
+import jdk.nashorn.internal.scripts.JS;
+import org.openqa.selenium.By;
+import org.openqa.selenium.JavascriptExecutor;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.BeforeClass;
+import org.testng.annotations.Test;
+import utilities.BrowserUtils;
+
+import java.sql.Driver;
+
+public class Scroll {
+    static WebDriver driver;
+
+    @BeforeClass
+    private void before() {
+        WebDriverManager.chromedriver().setup();
+        driver = new ChromeDriver();
+    }
+
+    @Test
+    public void scrollDown() throws InterruptedException {
+        driver.manage().window().maximize();
+        driver.get("http://practice.cybertekschool.com/infinite_scroll");
+        JavascriptExecutor JS = (JavascriptExecutor) driver;
+        Thread.sleep(1000);
+        JS.executeScript("window.scrollBy(0,500)");
+        Thread.sleep(1000);
+        // or
+        //JS.executeScript("scroll(0,1000);");
+        driver.close();
+
+    }
+
+    @Test
+    public void scrollToParticularElement(){
+        driver.manage().window().maximize();
+        driver.get("http://practice.cybertekschool.com/infinite_scroll");
+
+        JavascriptExecutor js = (JavascriptExecutor) driver;
+        for (int i = 0; i < 5; i++) {
+            BrowserUtils.wait(1);
+            js.executeScript("window.scrollBy(0,1000)");
+        }
+        WebElement element = driver.findElement(By.xpath("//a[.='Home']"));
+        //((JavascriptExecutor) driver).executeScript("arguments[0].scrollIntoView();", element);
+        js.executeScript("arguments[0].scrollIntoView();", element);
+        driver.close();
+    }
+
+    @Test
+    public void scrollingInfinite() {
+        driver.manage().window().maximize();
+        driver.get("http://practice.cybertekschool.com/infinite_scroll");
+        JavascriptExecutor js = (JavascriptExecutor) driver;
+        BrowserUtils.wait(2);
+
+        for (int i = 0; i < 10; i++) {
+            BrowserUtils.wait(1);
+            js.executeScript("window.scrollBy(0,300)");
+        }
+
+        for (int i = 0; i < 5; i++) {
+            BrowserUtils.wait(1);
+            js.executeScript("window.scrollBy(0,1000)");
+        }
+        driver.close();
+    }
+}
